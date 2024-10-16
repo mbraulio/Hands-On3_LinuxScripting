44d12a8a60ba83761e0fc8c48d30b27f959a001f
mbraulio <braulio.montes21@gmail.com>
2024-10-15 22:47:55 -0600

    Scripts

---
+++ helloworld.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo -e "\n Hello World \n"
+
+echo -e "\n Archivos en el directorio actual: \n"
+
+mkdir -p Test
+
+cd Test
+
+echo -e "\n Archivos en el directorio 'Test': \n"
+
+ls
+
